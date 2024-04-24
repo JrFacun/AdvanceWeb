74dce7039495c85b03b667839717eb1c14707fd4
Jr Facun <98105196+JrFacun@users.noreply.github.com>
2024-04-24 16:50:01 +0800

    Login & Signup working

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import Home from "./Components/Home";
 import Login from './Components/Login';
 import Register from './Components/Signup';
 import Contact from './Components/Contact';
+import NotFound from './Components/NotFound';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route exact path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Register />} />
           <Route path="/contacts" element={<Contact />} />
-         
+          <Route path="/notfound" element={<NotFound />} />
         </Routes>
       </div>
     </Router>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
 export const Navbar = () => {
     return(
         <div>
             <Link to="signup">Signup - Up</Link>
             <Link to="login">Login</Link>
-            <Link to="/">Home</Link>
-            <Link to="/contacts">Contact</Link>
         </div>
     )
 }
