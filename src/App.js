1fc61f8052972b1412ac9d7bd282750560f231f7
Jr Facun <98105196+JrFacun@users.noreply.github.com>
2024-04-30 19:27:31 +0800

    Add Products & Firebase modified for Storage

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import Login from './Components/Login';
 import Register from './Components/Signup';
-import Contact from './Components/Contact';
 import NotFound from './Components/NotFound';
+import { AddProducts } from './Components/AddProducts';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route exact path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Register />} />
-          <Route path="/contacts" element={<Contact />} />
+          <Route path="/add-products" element={<AddProducts />} />
+          <Route exact path="/" element={<Home />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
