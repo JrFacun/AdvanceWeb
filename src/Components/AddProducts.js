705102509fc8d52f151a6dcd59ba429c9026e063
Jr Facun <98105196+JrFacun@users.noreply.github.com>
2024-05-15 01:04:40 +0800

    Header Add Product

--- src/Components/AddProducts.js
+++ src/Components/AddProducts.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { storage, fs } from '../Components/Config/Firebase'
 import { ref, getDownloadURL, uploadBytes } from 'firebase/storage'
 import { collection, addDoc, serverTimestamp } from 'firebase/firestore'
+import Header from "./Header";
 
 export const AddProducts = () => {
     const [title, setTitle] = useState('');
@@ -70,56 +71,59 @@ export const AddProducts = () => {
     }
 
     return (
-        <div className="container">
-            <br />
-            <br />
-            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                <h1>Add Products</h1>
-                <Link to="/home" className="btn btn-primary">Back to Home</Link>
-            </div>
-            <hr />
+        <div>
+            <Header />
+            <div className="container">
 
-            {successMsg && <>
-                <div className="success-msg">{successMsg}</div>
-            </>}
-            <form autoComplete="off" className="form-group" onSubmit={handleAddProducts}>
-                <label>Product Title</label>
-                <input type="text" className="form-control" required
-                    onChange={(e) => setTitle(e.target.value)} value={title}></input>
                 <br />
-                <label>Product Description</label>
-                <input type="text" className="form-control" required
-                    onChange={(e) => setDescription(e.target.value)} value={description}></input>
-                <br />
-                <label>Product Price</label>
-                <input type="number" className="form-control" required
-                    onChange={(e) => setPrice(e.target.value)} value={price}></input>
-                <br />
-                <label>Product Qty</label>
-                <input type="number" className="form-control" required
-                    onChange={(e) => setQty(e.target.value)} value={qty}></input>
-                <br />
-                <label>Upload Product Image</label>
-                <input type="file" id="file" className="form-control" required
-                    onChange={handleProductsImg}></input>
-                {imageUrl && <img src={imageUrl} alt="Preview" style={{ maxWidth: '25%', maxHeight: '25%', marginTop: '10px', marginBottom: '10px', marginLeft: '500px' }} />}
-                {imageError && <>
-                    <br />
-                    <div className="error-msg">{imageError}</div>
-                </>}
-                <br />
-
-                <div style={{ display: "flex", justifyContent: 'flex-end' }}>
-                    <button type="submit" className="btn btn-success btn-md">
-                        SUBMIT
-                    </button>
+                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                    <h1>Add Products</h1>
+                    <Link to="/home" className="btn btn-primary">Back to Home</Link>
                 </div>
-                <br></br>
-            </form>
-            {uploadError && <>
-                <br />
-                <div className="error-msg">{uploadError}</div>
-            </>}
+                <hr />
+
+                {successMsg && <>
+                    <div className="success-msg">{successMsg}</div>
+                </>}
+                <form autoComplete="off" className="form-group" onSubmit={handleAddProducts}>
+                    <label>Product Title</label>
+                    <input type="text" className="form-control" required
+                        onChange={(e) => setTitle(e.target.value)} value={title}></input>
+                    <br />
+                    <label>Product Description</label>
+                    <input type="text" className="form-control" required
+                        onChange={(e) => setDescription(e.target.value)} value={description}></input>
+                    <br />
+                    <label>Product Price</label>
+                    <input type="number" className="form-control" required
+                        onChange={(e) => setPrice(e.target.value)} value={price}></input>
+                    <br />
+                    <label>Product Qty</label>
+                    <input type="number" className="form-control" required
+                        onChange={(e) => setQty(e.target.value)} value={qty}></input>
+                    <br />
+                    <label>Upload Product Image</label>
+                    <input type="file" id="file" className="form-control" required
+                        onChange={handleProductsImg}></input>
+                    {imageUrl && <img src={imageUrl} alt="Preview" style={{ maxWidth: '25%', maxHeight: '25%', marginTop: '10px', marginBottom: '10px', marginLeft: '500px' }} />}
+                    {imageError && <>
+                        <br />
+                        <div className="error-msg">{imageError}</div>
+                    </>}
+                    <br />
+
+                    <div style={{ display: "flex", justifyContent: 'flex-end' }}>
+                        <button type="submit" className="btn btn-success btn-md">
+                            SUBMIT
+                        </button>
+                    </div>
+                    <br></br>
+                </form>
+                {uploadError && <>
+                    <br />
+                    <div className="error-msg">{uploadError}</div>
+                </>}
+            </div>
         </div>
     )
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -56,6 +56,7 @@ function Header() {
                   Logout
                 </Link>
               </li>
+
             </ul>
           </div>
         </div>
